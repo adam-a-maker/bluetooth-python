93926c3b303f818f205856b86c9be679dee87ceb
adam-a-maker <dam.abd000@gmail.com>
2025-12-19 22:06:31 +0100

    Update bluetooth-scan.py

--- bluetooth-scan.py
+++ bluetooth-scan.py
@@ -8,6 +8,8 @@ from bleak import BleakScanner, BleakClient
 
 TARGET_NAME = "JBL LIVE660NC-LE"  # Replace with your target device name
 TARGET_ADRESS = None  # Will be set when the target device is found
+NB_RETRIES = 5
+TRIES = 1
 
 async def bluetooth_power(turn_on):
     all_radios = await radios.Radio.get_radios_async()
@@ -29,18 +31,21 @@ async def main():
             
     if TARGET_ADRESS is None:
         print(f"Target device {TARGET_NAME} not found. Retrying in 5 seconds...")
+        return False
     else:
         print(f"Found target device: {TARGET_NAME} at {TARGET_ADRESS}")
         async with BleakClient(TARGET_ADRESS) as client:
             # we’ll do the read/write operations here
-            print(f"Succesfully connected to {TARGET_NAME}")
+            print(f"Succesfully connected to {TARGET_NAME} in {TRIES} tries.")
             print(client.is_connected)
             return True
             
 if __name__ == '__main__':
     asyncio.run(bluetooth_power(True)) # Ensure Bluetooth is on
-while True:
-    print("Scanning for Bluetooth LE devices...")
-    asyncio.run(main())
-    if asyncio.run(main()) == True:
-        break # Exit loop if connected
+    for i in range(0, NB_RETRIES):
+        print("Scanning for Bluetooth LE devices...")
+        stop = asyncio.run(main())
+        if stop:
+            break # Exit loop if connected
+        TRIES += 1
+        
